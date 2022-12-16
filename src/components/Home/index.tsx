6fd9a62e21d82a0708578d2ac29c3c11aeffb801
TanjimAnim <tanjimanim007@gmail.com>
2022-12-17 00:46:36 +0600

    feat:home page component

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,32 @@
+import Header from "../Header";
+import Navbar from "../navbar";
+import { Box } from "@chakra-ui/react";
+
+//import assets
+import pattern1 from "../../assets/pattern.png";
+import pattern2 from "../../assets/patternBG.png";
+
+export default function HomePage() {
+  return (
+    <Box id="homepage" background="black">
+      <Navbar />
+      <Header />
+      <Box
+        position="absolute"
+        width="368px"
+        height="324px"
+        left="0px"
+        top="0px"
+        backgroundImage={pattern1.src}
+      />
+      <Box
+        position="absolute"
+        width="1436px"
+        height="846px"
+        left="672px"
+        top="151px"
+        backgroundImage={pattern2.src}
+      />
+    </Box>
+  );
+}
