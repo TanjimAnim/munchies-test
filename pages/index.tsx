712d0a2ef4a37312cbc2bf3feb2a9164f15a1a73
TanjimAnim <tanjimanim007@gmail.com>
2022-12-18 02:04:02 +0600

    feat:added category component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Category from "../src/components/category";
 import HomePage from "../src/components/Home";
 
 export default function Home() {
   return (
     <div>
       <HomePage />
+      <Category />
     </div>
   );
 }
