c4ab85f7f98c19007ff50c9e6d02735f711e86a2
TanjimAnim <tanjimanim007@gmail.com>
2022-12-18 20:37:12 +0600

    feat:added new components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import Category from "../src/components/category";
 import HomePage from "../src/components/Home";
+import Map from "../src/components/map";
+import Menu from "../src/components/menu";
+import Fondas from "../src/components/Fondas";
+import TopFooter from "../src/components/Footer/TopFooter";
+import BottomFooter from "../src/components/Footer/BottomFooter";
 
 export default function Home() {
   return (
     <div>
       <HomePage />
       <Category />
+      <Map />
+      <Menu />
+      <Fondas />
+      <TopFooter />
+      <BottomFooter />
     </div>
   );
 }
