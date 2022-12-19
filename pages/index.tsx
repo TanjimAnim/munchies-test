b99f8057cad4732015320092f13301bd5104490a
TanjimAnim <tanjimanim007@gmail.com>
2022-12-20 01:48:10 +0600

    feat:added components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Menu from "../src/components/menu";
+import HomePage from "../src/components/Home";
 
 export default function Home() {
   return (
     <div>
+      <HomePage />
       <Menu />
     </div>
   );
