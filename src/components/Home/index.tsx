9103ac658b9e534d63feb4462acc277116422c6d
TanjimAnim <tanjimanim007@gmail.com>
2022-12-20 01:49:00 +0600

    feat:edited layout

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -8,25 +8,9 @@ import pattern2 from "../../assets/patternBG.png";
 
 export default function HomePage() {
   return (
-    <Box id="homepage" background="black">
+    <Box id="homepage" background="#0C1712">
       <Navbar />
       <Header />
-      <Box
-        position="absolute"
-        width="368px"
-        height="324px"
-        left="0px"
-        top="0px"
-        backgroundImage={pattern1.src}
-      />
-      <Box
-        position="absolute"
-        width="1436px"
-        height="774px"
-        left="672px"
-        top="151px"
-        backgroundImage={pattern2.src}
-      />
     </Box>
   );
 }
