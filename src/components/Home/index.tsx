af2f323e1d75b02f744d22f41f6f2df99862cbf8
TanjimAnim <tanjimanim007@gmail.com>
2022-12-20 18:41:37 +0600

    removed assets

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,10 +2,6 @@ import Header from "../Header";
 import Navbar from "../navbar";
 import { Box } from "@chakra-ui/react";
 
-//import assets
-import pattern1 from "../../assets/pattern.png";
-import pattern2 from "../../assets/patternBG.png";
-
 export default function HomePage() {
   return (
     <Box id="homepage" background="#0C1712">
