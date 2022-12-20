a93842ced7b94b28961c0a27457dac3d93fb0b3d
TanjimAnim <tanjimanim007@gmail.com>
2022-12-20 18:41:50 +0600

    feat: foodcard component

--- src/components/menu/foodCard.tsx
+++ src/components/menu/foodCard.tsx
@@ -1,9 +1,96 @@
-import { Box } from "@chakra-ui/react";
+import { Box, chakra, Image, Text } from "@chakra-ui/react";
 import { FoodType } from "../../../types";
-import { munchiesApi, useGetAllProductsQuery } from "../../services/products";
+import { useGetAllProductsQuery } from "../../services/products";
+
+//import assets
+import addToCartImage from "../../assets/add-to-cart.png";
 
 export default function FoodCard() {
-  const productsQuery = useGetAllProductsQuery("");
-  console.log(productsQuery)
-  return <Box marginTop="40px"></Box>;
+  const FoodData = useGetAllProductsQuery("").data;
+  const { isLoading, isError } = useGetAllProductsQuery("");
+
+  return (
+    <Box
+      marginTop="40px"
+      marginLeft="265px"
+      display="flex"
+      flexWrap="wrap"
+      gap="24px"
+    >
+      {isError ? (
+        <Box>
+          {" "}
+          <Text> Oh no there was an error</Text>
+        </Box>
+      ) : isLoading ? (
+        <Box></Box>
+      ) : (
+        FoodData.data.map((item: FoodType) => {
+          return (
+            <Box key={item.id} borderRadius="10px">
+              <Image
+                src={item.image}
+                width="277px"
+                height="250px"
+                borderRadius="10px 10px 0px 0px"
+              />
+              <Box
+                background="white"
+                padding="15px 12px 15px 12px"
+                boxShadow="0px 0px 1px rgba(187, 187, 187, 0.25)"
+                borderRadius="0px 0px 10px 10px"
+              >
+                <Box display="flex" justifyContent="space-between">
+                  <Text
+                    fontSize="16px"
+                    fontWeight={400}
+                    letterSpacing="-0.012em"
+                    color="#1B1C21"
+                  >
+                    {item.name}
+                  </Text>
+                  <Text
+                    fontSize="16px"
+                    fontWeight={400}
+                    letterSpacing="-0.012em"
+                    color="#1B1C21"
+                  >
+                    {item.price} <chakra.span fontWeight={700}>BDT</chakra.span>
+                  </Text>
+                </Box>
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  marginTop="16px"
+                >
+                  <Box display="flex" gap="12px">
+                    <Box
+                      padding="0px 4px"
+                      color="#1B1C21"
+                      background="#F7F8FA"
+                      boxShadow="0px 0px 1px rgba(0, 0, 0, 0.25)"
+                      borderRadius="5px"
+                    >
+                      Qty:{item.quantity_available}
+                    </Box>
+                    <Box
+                      padding="0px 4px"
+                      background="#F7F8FA"
+                      borderRadius="5px"
+                      color="#1B1C21"
+                    >
+                      50-79 min
+                    </Box>
+                  </Box>
+                  <Box>
+                    <Image src={addToCartImage.src} />
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+          );
+        })
+      )}
+    </Box>
+  );
 }
