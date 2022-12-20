3d8ab364499838bbb8f17c98f1bee731fd364766
TanjimAnim <tanjimanim007@gmail.com>
2022-12-20 18:40:20 +0600

    feat: added components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import Menu from "../src/components/menu";
 import HomePage from "../src/components/Home";
+import Blog from "../src/components/Blog";
+import Contact from "../src/components/Contact";
+import Footer from "../src/components/Footer";
 
 export default function Home() {
   return (
     <div>
       <HomePage />
       <Menu />
+      <Blog />
+      <Contact />
+      <Footer />
     </div>
   );
 }
