f4f0145e0ce382e6de127bc5056483d734d56f3c
TanjimAnim <tanjimanim007@gmail.com>
2022-12-22 01:36:47 +0600

    feat: added methods

--- src/features/slice.ts
+++ src/features/slice.ts
@@ -1,37 +1,72 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { isTemplateSpan } from "typescript";
 import { FoodCartType } from "../../types";
 
-type CartState = {
-  cart: FoodCartType[];
+export type CartState = {
+  items: FoodCartType[];
 };
 
 const initialState: CartState = {
-  cart: [],
+  items: [],
 };
 
 export const foodcartSlice = createSlice({
   name: "foodcart",
-  initialState,
+  initialState: initialState,
   reducers: {
     addToCart: (state, action) => {
-      const itemIndex = state.cart.findIndex(
+      const itemIndex = state.items.findIndex(
         (item) => item.id === action.payload.id
       );
 
       if (itemIndex >= 0) {
-        state.cart[itemIndex].quantity += 1;
-        state.cart[itemIndex].quantity_available -= 1;
+        if (state.items[itemIndex].quantity_available === 0) {
+          return state;
+        }
+        state.items[itemIndex].quantity += 1;
+        state.items[itemIndex].quantity_available -= 1;
+        state.items[itemIndex].total_price =
+          (state.items[itemIndex].price + state.items[itemIndex].vat) *
+          state.items[itemIndex].quantity;
       } else {
-        state.cart.push({
+        state.items.push({
           ...action.payload,
           quantity: 1,
           quantity_available: action.payload.quantity_available - 1,
+          total_price: action.payload.price + action.payload.vat,
         });
       }
+    },
+    // reloadCart: (state, action) => {
+    //   console.log("state", state, action);
+    //   return {
+    //     ...state,
+    //     ...JSON.parse(localStorage.getItem("persist:cart")),
+    //   };
+    // },
+    initialCartValue: (state, action) => {
+      return {
+        ...state,
+        items: action.payload,
+      };
+    },
+    clearCart: (state) => {
+      return {
+        ...state,
+        items: [],
+      };
+    },
+    removeCart: (state, action) => {
+      console.log(action);
+      return {
+        ...state,
+        items: state.items.filter((elem) => elem.id !== action.payload),
+      };
     },
   },
 });
 
-export const { addToCart } = foodcartSlice.actions;
+export const { addToCart, initialCartValue, clearCart, removeCart } =
+  foodcartSlice.actions;
 
 export default foodcartSlice.reducer;
