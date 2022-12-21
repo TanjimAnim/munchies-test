044662082ee76af3daea24bbebe9e0ceecfcac1e
TanjimAnim <tanjimanim007@gmail.com>
2022-12-22 01:35:23 +0600

    feat: added persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { foodcartSlice } from "../features/slice";
+
 import { munchiesApi } from "../services/products";
+
+//import redux persist
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import useFoodCartReducer from "../features/slice";
+
+const persistConfig = {
+  key: "cart",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, useFoodCartReducer);
 
 export const store = configureStore({
   reducer: {
     [munchiesApi.reducerPath]: munchiesApi.reducer,
-    cart: foodcartSlice.reducer,
+    cart: persistedReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(munchiesApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(munchiesApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
