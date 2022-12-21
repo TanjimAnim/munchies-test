2525802fad2d86c6a8586545a048b04e6d2bffd2
TanjimAnim <tanjimanim007@gmail.com>
2022-12-22 01:33:22 +0600

    feat: added persist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,16 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../src/app/store";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+let persistor = persistStore(store);
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Provider store={store}>
-        <Component {...pageProps} />
+        <PersistGate persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
       </Provider>
     </ChakraProvider>
   );
